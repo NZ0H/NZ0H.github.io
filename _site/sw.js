d84dd117984452cb16d9e457857763792334a165
Enzo <NZ0H@users.noreply.github.com>
2023-09-01 22:58:55 +0200

    Modification photo onglet

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230901.221751';
+const cacheName = 'chirpy-20230901.224903';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
