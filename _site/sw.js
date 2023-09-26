cae4bed29481ba56bb5ba1f3db8ababe00627731
Enzo <NZ0H@users.noreply.github.com>
2023-09-26 22:14:30 +0200

    Probleme index telnet

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230926.220953';
+const cacheName = 'chirpy-20230926.221356';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
